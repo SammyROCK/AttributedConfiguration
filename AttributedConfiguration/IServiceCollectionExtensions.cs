06d8bb28715646ddf325b3ff8cc0d4407485930e
SammyROCK <samfmf@gmail.com>
2021-06-25 15:58:25 -0300

    Added AddAttributedConfigurationsFromAssemblyContaining

--- AttributedConfiguration/IServiceCollectionExtensions.cs
+++ AttributedConfiguration/IServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using AttributeSdk;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Linq;
 
@@ -12,10 +13,26 @@ namespace AttributedConfiguration {
 					?? type.GetInterfaces().FirstOrDefault()
 					?? type;
 
-				serviceCollection.AddSingleton(
+				serviceCollection.TryAdd(new ServiceDescriptor(
 					serviceType,
-					serviceProvider => configuration.Resolve(type, configureAttribute)
-				);
+					serviceProvider => configuration.Resolve(type, configureAttribute),
+					ServiceLifetime.Singleton
+				));
+			}
+			return serviceCollection;
+		}
+
+		public static IServiceCollection AddAttributedConfigurationsFromAssemblyContaining<T>(this IServiceCollection serviceCollection, IConfiguration configuration) {
+			foreach(var (type, configureAttribute) in typeof(T).Assembly.EnumerateTypesWithAttribute<ConfigureAttribute>()) {
+				var serviceType = configureAttribute.ServiceType
+					?? type.GetInterfaces().FirstOrDefault()
+					?? type;
+
+				serviceCollection.TryAdd(new ServiceDescriptor(
+					serviceType,
+					serviceProvider => configuration.Resolve(type, configureAttribute),
+					ServiceLifetime.Singleton
+				));
 			}
 			return serviceCollection;
 		}
